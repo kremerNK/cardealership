2b50f18c870bffa98f9784ceaa40b2b6f655ab44
kremer <easton08@protonmail.com>
2020-03-08 21:45:03 -0500

    march 8 2020

--- car/static/js/home_page.js
+++ car/static/js/home_page.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     $('#slideshowcontainer .slides').slick({
         slidesToShow: 4,
         slidesToScroll: 1, 
-        autoplay: false,
+        autoplay: true,
         adaptiveHeight: true,
         autoplaySpeed: 2000, 
         responsive: [
@@ -56,82 +56,4 @@ $(document).ready(function(){
         ]
         
     })
-    
-    
 });
-
-
-
-
-
-// let slideInterval = setInterval('slideFunction(slideCount)', 2000);
-
-
-
-
-// function slideFunction(){
-//     if (slideCount < slidePics.length - 3){
-//         slideCount++;
-//     } else {
-//         slideCount = 0
-//     };
-
-//     let emptyArray = [];
-
-
-//     for (i=slideCount; i < slideCount + 3; i++){
-//         emptyArray.push(i)
-//     }
-
-//     console.log(emptyArray);
-    
-//     // for (i=0; i < emptyArray.length; i++)
-//     slidePics.forEach(e1 => e1.style.display = 'none');
-
-//     slidePics[emptyArray[0]].style.display = '';
-//     slidePics[emptyArray[1]].style.display = '';
-//     slidePics[emptyArray[2]].style.display = '';
-        
-
-//     // slidePics.forEach(e1 => {
-//     //     // console.log(e1);
-        
-        
-//     //     // for (i=0; i < emptyArray.length; i++){
-//     //     //    console.log(e1);
-           
-            
-            
-            
-            
-            
-//     //     //     if (e1 == slidePics[emptyArray[i]]){
-                
-//     //     //         console.log('true');
-                
-                
-//     //     //         e1.style.display = '';
-//     //     //     } 
-//     //     //     else {
-//     //     //         e1.style.display = 'none';
-//     //     //     }
-            
-//     //     // }
-//     // })
-
-
-//     //     if (slideCount < slidePics.length - 1){
-//     //         slideCount++;
-//     //     } else {
-//     //         slideCount = 0;
-//     //     }
-
-//     //     if (e1 == slidePics[slideCount]){
-//     //         e1.style.display = '';
-//     //     } else {
-//     //         e1.style.display = 'none';
-//     //     }
-//     // })
-//     };
-    
-    
