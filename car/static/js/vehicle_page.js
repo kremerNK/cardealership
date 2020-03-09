06c1ba76d0aeea3c2c2458fa534221848a8e0af9
kremer <easton08@protonmail.com>
2020-03-08 22:12:59 -0500

    march 8 2020

--- car/static/js/vehicle_page.js
+++ car/static/js/vehicle_page.js
@@ -1,1 +1,12 @@
- 
+$(document).ready(function(){
+    $('.image .slides').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1, 
+        autoplay: false,
+        adaptiveHeight: true,
+        autoplaySpeed: 2000, 
+        
+    })
+});
+
+console.log('test');
