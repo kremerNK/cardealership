aafdd6a44a95936690ba575c0f6cdbab46696e96
kremer <easton08@protonmail.com>
2020-03-15 13:49:59 -0500

    march 12 2020

--- car/static/js/base.js
+++ car/static/js/base.js
@@ -5,30 +5,6 @@ navBarToggle.addEventListener('click', function () {
     mobileNumber.classList.toggle('hidephone');
     mainNav.classList.toggle('active');
 }); 
-    
-
-
-
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//         document.getElementsByClassName('topbanner')[0].style.height = '8vh';
-//         document.getElementById('leftbannerh4').style.display = 'none';
-//         document.getElementsByClassName('rightbanner')[0].style.flexDirection = 'row';
-//         document.getElementById('phonenumber').style.paddingRight = '50px';
-//         document.getElementsByClassName('rightbanner')[0].style.alignItems = 'center';
-        
-
-         
-//     } else {
-//         document.getElementsByClassName('rightbanner')[0].style.flexDirection = 'column';
-//         document.getElementsByClassName('topbanner')[0].style.height = '15vh';
-//         document.getElementById('leftbannerh4').style.display = 'block';
-//         document.getElementById('phonenumber').style.paddingRight = '0px';
-//         document.getElementsByClassName('rightbanner')[0].style.alignItems = 'flex-end';
-//     }
-// }; 
 
 
 window.onscroll = function () {myFunction()};
---
+++ car/static/js/contact.js
@@ -0,0 +1,8 @@
+let bannerHeight = document.querySelector('.top-banner').offsetHeight;
+let phoneHeight = document.querySelector('.mobile-phone').offsetHeight;
+let marginTop = bannerHeight + phoneHeight;
+let body = document.querySelector('.mobile-margin');
+
+
+
+body.style.marginTop = ((marginTop * 1.1).toString() + 'px');
