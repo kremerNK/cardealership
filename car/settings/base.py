6f847b27625ac110cb25812f08d0c13dc57ab014
kremer <easton08@protonmail.com>
2020-03-24 15:29:07 -0500

    a

--- car/settings/base.py
+++ car/settings/base.py
@@ -172,4 +172,15 @@ EMAIL_HOST_USER = 'glycine775@gmail.com'
 EMAIL_HOST_PASSWORD = '/Forget12'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False 
+EMAIL_USE_SSL = True 
+
+
+
+#original settings
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'glycine775@gmail.com'
+# EMAIL_HOST_PASSWORD = '/Forget12'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False 
