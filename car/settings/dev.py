11e1e48c0026818344b990295eeefc89ffbde938
kremer <easton08@protonmail.com>
2020-04-05 15:14:40 -0500

    a

--- car/settings/dev.py
+++ car/settings/dev.py
@@ -12,7 +12,8 @@ print(testkey)
 DEBUG = True
  
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'm^@pmbe5h!75o(!$lfwbb3y_$oi2bipk_l5)(b!f&f)(oigc9z'
 
 
 # SECURITY WARNING: define the correct hosts in production!
