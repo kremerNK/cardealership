8e1cfd078b8fa2f43c00e4d4666b333b2346b545
kremer <easton08@protonmail.com>
2020-04-05 13:26:56 -0500

    april 5 2020

--- car/settings/dev.py
+++ car/settings/dev.py
@@ -1,7 +1,8 @@
 from .base import *
 
+print('development')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
@@ -17,4 +18,3 @@ try:
     from .local import *
 except ImportError:
     pass
-    
--- car/settings/production.py
+++ car/settings/production.py
@@ -1,7 +1,22 @@
 from .base import *
+from dotenv import load_dotenv
 
+print('production')
+# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv("SECRET_KEY")
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
+
+# SECURITY WARNING: define the correct hosts in production!
+# ALLOWED_HOSTS = ['192.168.0.4', 'localhost', 'straussweb.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
+
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 try:
     from .local import *
 except ImportError:
