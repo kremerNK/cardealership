bedb3aac0e0846c1b38be1716dfb7eb69093e16d
kremer <easton08@protonmail.com>
2020-04-07 18:32:56 -0500

    april 7 2020

--- carlist/models.py
+++ carlist/models.py
@@ -25,7 +25,7 @@ class Vehicle(Page):
         van = 'Van'
    
     model = models.CharField(max_length=200, null=True, default='1')
-   
+     
     year = models.CharField(max_length=200, null=True, default='')
     make = models.CharField(max_length=200, null=True, default='toyota')
     price = models.CharField(max_length=200, null=True, default='$')
@@ -43,12 +43,31 @@ class Vehicle(Page):
     vin = models.CharField(max_length=200, default='1')
     picture = models.ImageField(default='')
 
-    @property
-    def testname(self):
-        return '{} {}'.format(self.mileage, '+++1')
 
-    def child_pages(self):
-        return ItemPage.objects.live().child_of(self)
+    # print(vars(object))
+    # @property
+    # def testname(self):
+    #     return '{} {}'.format(self.mileage, '+++1')
+
+    # def child_pages(self):
+    #     return ItemPage.objects.live().child_of(self)
+    print('self')
+    def get_url_parts(self, *args, **kwargs):
+        # url_parts = super(Vehicle, self).get_url_parts(*args, **kwargs)
+
+        # if url_parts is None:
+        #     # in this case, the page doesn't have a well-defined URL in the first place -
+        #     # for example, it's been created at the top level of the page tree
+        #     # and hasn't been associated with a site record
+        #     return None
+
+        # site_id, root_url, page_path = url_parts
+        site_id = ''
+        root_url = ''
+        page_path = '/search-vehicles/' + self.slug.replace(' ', '-').lower() + '-' + str(self.id)
+
+        # return '/' in place of the real page path
+        return (site_id, root_url, page_path)
 
     content_panels = Page.content_panels + [
         
@@ -72,6 +91,7 @@ class Vehicle(Page):
         InlinePanel('gallery_images', label="Gallery images")
     ]
 
+    
 
 class VehicleGallery(Orderable):
     page = ParentalKey(Vehicle, related_name='gallery_images')
