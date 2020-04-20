2068ba167801ead5a08030c77915dfec23ec4e9f
kremer <easton08@protonmail.com>
2020-04-19 20:40:12 -0500

    april 19 2020

--- home/static/js/home_page.js
+++ home/static/js/home_page.js
@@ -1,5 +1,13 @@
+let background1 = document.querySelector('.background1').getBoundingClientRect();
+let bgtest = document.querySelector('.mybg2')
 
-let searchForm = document.querySelector('.search-form').getBoundingClientRect().height * .50;
+
+let rightTrack = document.querySelector('.mybg');
+let leftTrack = document.querySelector('.mybg2');
+
+
+
+// let searchForm = document.querySelector('.search-form').getBoundingClientRect().height * .50;
 
 
 //set length of background image tire tracks////
@@ -13,13 +21,75 @@ let navBarHeight = document.querySelector('#navbar').getBoundingClientRect();
 let navHeight = document.querySelector('#navbar').getBoundingClientRect().height;
 let bannerHeight = document.querySelector('.leftbanner').getBoundingClientRect().height;
 
-bg.style.height = (((navHeight + bannerHeight + introBounding.offsetTop + introBounding.offsetHeight) * 1.1)).toString().concat('px');
-bg2.style.height = (((navHeight + bannerHeight + introBounding.offsetTop + introBounding.offsetHeight) * 1.1)).toString().concat('px');
+let testbg = document.querySelector('.mybg').getBoundingClientRect();
+let testbg2 = document.querySelector('.mybg2').getBoundingClientRect();
+holder = 0;
+window.onload = function(){
+    footer = document.querySelector('.footer').offsetTop;
+    console.log(footer);
+    bg.style.height = (footer + (introBounding.offsetHeight * 0.6)).toString().concat('px');
+    bg2.style.height = (footer + (introBounding.offsetHeight * 0.6)).toString().concat('px');
+    bvavg = ((testbg.x + testbg2.x) / 2);
+    console.log(Math.abs(bvavg));
+    
+
+    window.addEventListener('scroll', function(){
+        console.log('roll');
+        let car = document.querySelector('#svg2')
+        bg.classList.add('roll');
+        bg2.classList.add('roll');
+        roll = document.querySelectorAll('.roll');
+        roll.forEach(e1 => e1.style.maxHeight = (footer + Math.abs(testbg2.x)).toString().concat('px'));
+        console.log(footer);
+        //wait X seconds for roll animation
+        setTimeout(function(){
+            holder += 1;
+            console.log(holder);
+            if (holder <= 1){
+                car.style.visibility = 'hidden';
+                car.style.height = '0px';
+                // car.style.display = 'none';
+
+            }
+            
+        }, 10000)
+            
+
+        
+        
+    })
+}
+
+//fade in divs//
+//.search-form, #slideshowcontainer, .intro
+
+// let searchForm = document.querySelector('.search-form');
+// let slideShowContainer = document.querySelector('#slideshowcontainer');
+// let introDiv = document.querySelector('.intro');
+
+// function fadeIn(element){
+//     op = 0.1;
+//     element.style.display = 'flex';
+//     var timer = setInterval(function () {
+//         if (op >= 1){
+//             clearInterval(timer);
+//         }
+//         element.style.opacity = op;
+//         element.style.opacity = 'alpha(opacity=' + op * 100 + ")";
+//         op += op * 0.1;
+//     }, 20);
+// }
+
+// window.addEventListener('scroll', function(){
+//     // console.log(window.pageYOffset);
+//     console.log(searchForm.getBoundingClientRect().bottom + searchForm.getBoundingClientRect().height);
+    
+    
+// })
+
+//tire transition//
 
 
- 
-
-console.log((((navHeight + bannerHeight + introBounding.offsetTop + introBounding.offsetHeight) * 1.1)).toString().concat('px'));
 
 
 
